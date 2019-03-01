3cf9faade607a3dc8873f4e0950c0f2d828fe84e
Luke <Luke@192.168.1.71>
2019-03-01 23:35:33 +1300

    added question class + test question need to change char answer to int

--- src/main/PDCProject.java
+++ src/main/PDCProject.java
@@ -1,10 +1,29 @@
 package main;
 
+import java.util.Scanner;
+
 public class PDCProject 
 {
     public static void main(String[] args) 
     {
-
+        String test1 = "Whats my cats name????";
+        String[] test2 = new String[]{"1) goose","2) ellie","3) dog","4) shit"};
+        char test3 = 'd';
+        
+        Scanner scan = new Scanner(System.in);
+        
+        Question testQuestion = new Question(test1,test2,test3);
+        testQuestion.printQuestion();
+        int response = scan.nextInt();
+        
+        if (response == 4)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
     }
 
 }
