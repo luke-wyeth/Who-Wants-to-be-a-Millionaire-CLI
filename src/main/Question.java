0cdd5d13a61e23882dd26cb298d994fe6c92559f
Luke <Luke@192.168.1.71>
2019-03-03 19:24:06 +1300

    added question class + test question need to change char answer to int

---
+++ src/main/Question.java
@@ -0,0 +1,44 @@
+
+package main;
+
+import java.io.Serializable;
+
+public class Question implements Serializable
+{ 
+    // private and final to avoid tampering by other classes
+    private final String question;
+    private final String[] answers;
+    private final char correctAns;
+    
+    public Question(String question, String[] answers, char correctAns)
+    {
+        this.question = question;
+        this.answers = answers;
+        this.correctAns = correctAns;
+    }
+    
+    public String getQuestion()
+    {
+        return question;
+    }
+    
+    public String[] getAnswers()
+    {
+        return answers;
+    }
+    
+    public char getCorrectAns()
+    {
+        return correctAns;
+    }
+    
+    public void printQuestion()
+    {
+        System.out.println(question);
+        
+        for (int count = 0; count < 4; count++)
+        {
+            System.out.println(answers[count]);
+        }
+    }
+}
